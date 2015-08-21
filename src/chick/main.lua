492d040d1cab5690696b47d129aafa7c9ca53807
YouniS Bensalah <younis.bensalah@riseup.net>
2015-08-21 16:00:18 +0200

    main.lua in raw mode xD

--- src/chick/main.lua
+++ src/chick/main.lua
@@ -21,6 +21,11 @@ function love.load()
 
     love.window.setMode(1024, 800)
     love.window.setTitle('chick')
+
+    -- add module directories to lua path
+    package.path = "src/chick/core/?.lua;src/chick/net/?.lua;src/chick/net/?.lua;" .. package.path
+
+    test()
 end
 
 function love.update(dt)
@@ -32,4 +37,41 @@ end
 function love.conf(t)
     -- anti-aliasing
     t.window.fsaa = 4
+end
+
+function test()
+    require 'chick'
+    c = Chick.new(2)
+
+    local result = 1
+    while (result >= 0) do
+        local chain = io.read()
+        local chaintab = explode(' ', chain)
+        for i = 1, #chaintab do
+            chaintab[i] = tonumber(chaintab[i])
+        end
+        result = c:move(chaintab)
+        print('move ' .. chain)
+        print(result)
+    end
+end
+
+-- explode(seperator, string)
+-- Lance Li
+function explode(d,p)
+  local t, ll
+  t={}
+  ll=0
+  if(#p == 1) then return {p} end
+    while true do
+      l=string.find(p,d,ll,true) -- find the next d in the string
+      if l~=nil then -- if "not not" found then..
+        table.insert(t, string.sub(p,ll,l-1)) -- Save it in our array.
+        ll=l+1 -- save just after where we found it for searching next time.
+      else
+        table.insert(t, string.sub(p,ll)) -- Save what's left in our array.
+        break -- Break at end, as it should be, according to the lua manual.
+      end
+    end
+  return t
 end
