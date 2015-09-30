a949c0ea623d6aeb9ae985e572324147c6498c1c
YouniS Bensalah <younis.bensalah@riseup.net>
2015-09-30 14:24:43 +0200

    Add utils module: explode()

---
+++ src/chick/utils/explode.lua
@@ -0,0 +1,30 @@
+--
+-- This file is part of chick.
+--
+-- (c) 2015 YouniS Bensalah <younis.bensalah@riseup.net>
+--
+-- For the full copyright and license information, please view the LICENSE
+-- file that was distributed with this source code.
+--
+
+--
+-- explode(delimiter, string)
+-- Author: Lance Li
+--
+function explode(d, p)
+    local t, ll
+    t = {}
+    ll = 0
+    if (#p == 1) then return {p} end
+    while true do
+        l = string.find(p, d, ll, true) -- find the next d in the string
+        if l ~= nil then -- if "not not" found then..
+            table.insert(t, string.sub(p, ll, l-1)) -- Save it in our array.
+            ll=l+1 -- save just after where we found it for searching next time.
+        else
+            table.insert(t, string.sub(p, ll)) -- Save what's left in our array.
+            break -- Break at end, as it should be, according to the lua manual.
+        end
+    end
+    return t
+end
