ae30c46d731aac4da8ac9cf2bdf709da602208ff
YouniS Bensalah <younis.bensalah@gmail.com>
2016-10-06 03:38:34 +0200

    Rename even MORE stuff

---
+++ src/core/game.lua
@@ -0,0 +1,14 @@
+--[[
+-- This file is part of chicks.
+--
+-- (c) 2015-2016 YouniS Bensalah <younis.bensalah@gmail.com>
+--
+-- For the full copyright and license information, please view the LICENSE
+-- file that was distributed with this source code.
+--]]
+
+local Game = class('Game')
+
+function Game:initialize()
+
+end
